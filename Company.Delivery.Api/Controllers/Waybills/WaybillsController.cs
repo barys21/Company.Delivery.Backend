81abe41eb56e93cb898ff451df49482c0541518b
Kenes Kuanyshov <kuanyshovk@gmail.com>
2023-03-02 08:48:08 +0600

    Add project files.

---
+++ Company.Delivery.Api/Controllers/Waybills/WaybillsController.cs
@@ -0,0 +1,72 @@
+using Company.Delivery.Api.Controllers.Waybills.Request;
+using Company.Delivery.Api.Controllers.Waybills.Response;
+using Company.Delivery.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Delivery.Api.Controllers.Waybills;
+
+/// <summary>
+/// Waybills management
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class WaybillsController : ControllerBase
+{
+    private readonly IWaybillService _waybillService;
+
+    /// <summary>
+    /// Waybills management
+    /// </summary>
+    public WaybillsController(IWaybillService waybillService) => _waybillService = waybillService;
+
+    /// <summary>
+    /// Получение Waybill
+    /// </summary>
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(WaybillResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        // TODO: вернуть ответ с кодом 200 если найдено или кодом 404 если не найдено
+        // TODO: WaybillsControllerTests должен выполняться без ошибок
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Создание Waybill
+    /// </summary>
+    [HttpPost]
+    [ProducesResponseType(typeof(WaybillResponse), StatusCodes.Status200OK)]
+    public Task<IActionResult> CreateAsync([FromBody] WaybillCreateRequest request, CancellationToken cancellationToken)
+    {
+        // TODO: вернуть ответ с кодом 200 если успешно создано
+        // TODO: WaybillsControllerTests должен выполняться без ошибок
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Редактирование Waybill
+    /// </summary>
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(WaybillResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> UpdateByIdAsync(Guid id, [FromBody] WaybillUpdateRequest request, CancellationToken cancellationToken)
+    {
+        // TODO: вернуть ответ с кодом 200 если найдено и изменено, или 404 если не найдено
+        // TODO: WaybillsControllerTests должен выполняться без ошибок
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Удаление Waybill
+    /// </summary>
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        // TODO: вернуть ответ с кодом 200 если найдено и удалено, или 404 если не найдено
+        // TODO: WaybillsControllerTests должен выполняться без ошибок
+        throw new NotImplementedException();
+    }
+}
